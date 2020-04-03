917e778ae33831b2fb9080b6dcd9a7fb14111504
ygy <zero0_1one@163.com>
2020-04-03 16:26:08 +0800

    签名校验

--- example/upgrade/v1.0.js
+++ example/upgrade/v1.0.js
@@ -2,8 +2,8 @@
 
 //返回 upgrade 数组,按顺序执行, 只允许向后追加, 即使是取消上一步, 也应该是在最后追加一个 upgrade
 //本地开发过程中频繁变化, 请使用 autoSync 功能, 等测试完成后, 将 autoSync 的内容追加到 upgrade 中
-//所有 upgrade 执行都会在 `prefix_upgrade` 表中记录日志, 
-//如果异常就会终止, 后续不再执行, 下次执行(重启服务器) 会从上次异常处重新尝试.
+//所有 upgrade 执行都会在 `db_auto_migrate__upgrade` 表中记录日志, 
+//如果异常就会终止后续不再执行, 下次执行(重启服务器) 会从上次异常处重新尝试.
 module.exports = [
   //最简单的是使用一个 SQL 字符串, 
   'CREATE TABLE a (id int PRIMARY KEY, val int)',
@@ -40,8 +40,13 @@ module.exports = [
   {
     //与外部相同, 支持单语句,多语句字符串,数组或异步函数
     up: 'ALTER TABLE a RENAME TO aaa',
-    //指定校验签名,  执行完本条 upgrade 会进行校验, 如果失败将终止执行
-    sign: 'sha1|9a01c24317aebc687561242a427229da37c22dd5',
+
+    sign: {
+      //执行前的校验签名,  执行完本条 upgrade 前会进行校验, 如果失败将终止执行
+      begin: 'sha1|9071ad2edec03e446475200bfcb0c8cafaf108cc',
+      //执行后的校验签名,  执行完本条 upgrade 后会进行校验, 如果失败将终止后续 upgrade 执行
+      end: 'sha1|9a01c24317aebc687561242a427229da37c22dd5',
+    },
     //添加注释, 会记录进 `prefix_upgrade` 表中
     comment: 'RENAME TABLE a TO aaa',
   },
--- test/example_test.js
+++ test/example_test.js
@@ -1,8 +1,13 @@
 let example = require('../example/example')
-
+let Db = require('../lib/db')
+let options = require('../example/options')
 
 describe('example 测', function () {
   it('run example', async function () {
+    await Db.transaction(options.db, async db => {
+      await db.query(`DROP DATABASE IF EXISTS ${db.database}`)
+      await db.query(`CREATE DATABASE ${db.database}`)
+    })
     await example.run()
   })
 })
