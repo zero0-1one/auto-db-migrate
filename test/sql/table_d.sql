69dad01c91de1daf70e424a362695c7b9aab5c0f
ygy <zero0_1one@163.com>
2020-04-07 09:38:06 +0800

    新增对mysql8 索引降序的解析支持

--- test/sql/table_d.sql
+++ test/sql/table_d.sql
@@ -3,6 +3,6 @@ CREATE TABLE `d` (
   `d_value` int(11) NOT NULL,
   PRIMARY KEY (`d_id`),
   KEY `d_value` (`d_value`),
-  KEY `d_id` (`d_id', 'd_value`),
+  KEY `d_id` (`d_id', 'd_value` DESC),
   CONSTRAINT `d_ibfk_1` FOREIGN KEY (`d_id`) REFERENCES `table` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
