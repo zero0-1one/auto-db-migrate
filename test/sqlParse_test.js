29c8681c1ee2f68105b22fb9d810863b173f10da
ygy <zero0_1one@163.com>
2020-05-25 20:08:39 +0800

    解决当外键涉及的字段修改时， 由于外键约束，导致算法计算失败的 问题

--- test/sqlParse_test.js
+++ test/sqlParse_test.js
@@ -2,8 +2,6 @@ const fs = require('fs')
 const sqlParser = require('../lib/sqlParser')
 const { expect } = require('chai').use(require('chai-like'))
 
-
-
 describe('sqlParser 测试', function () {
   it('parseCreateSql()', function () {
     let sql = fs.readFileSync(__dirname + '/sql/table_d.sql', 'utf8')
@@ -12,19 +10,21 @@ describe('sqlParser 测试', function () {
       tableName: 'd',
       header: {},
       options: {
-        'ENGINE': "InnoDB",
-        'DEFAULT CHARSET': "utf8"
+        'ENGINE': 'InnoDB',
+        'DEFAULT CHARSET': 'utf8',
       },
       keys: {
         'primaryKey': { type: 'primaryKey', name: 'primaryKey', columns: [`d_id`] },
         'd_value': { type: 'key', name: 'd_value', columns: ['d_value'] },
         'd_id': { type: 'key', name: 'd_id', columns: ['d_id', 'd_value'] },
+      },
+      foreignKeys: {
         'd_ibfk_1': { type: 'foreignKey', name: 'd_ibfk_1', columns: ['d_id'], tableName: 'table', refColumns: ['id'] },
       },
       columns: {
         'd_id': { name: 'd_id', pos: 0, pre: null, next: { name: 'd_value' } },
-        'd_value': { name: 'd_value', pos: 1, pre: { name: 'd_id' }, next: null }
-      }
+        'd_value': { name: 'd_value', pos: 1, pre: { name: 'd_id' }, next: null },
+      },
     })
   })
 })
